fbe033001c1e923e3fd6794e6fd4278d63e6db3b
RuslanMamedov <41539922+RuslanMamedov@users.noreply.github.com>
2019-01-20 22:22:35 -0800

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Jan 15 13:23:43 2019
 
-@author: Mamedov
+@author: Mamedova
 """
 #Class Exercises - Variables and Instructions
 
@@ -26,23 +26,30 @@ DrawCircle('red',110,-25,radius)
 DrawCircle('yellow',-55,-75,radius)
 DrawCircle('green',55,-75,radius)'''
 
-#2.1 Convert Celsius to Fahrenheit
+assignment = eval(input ('Enter the assignment number:'))
 
-celsius = eval(input('Enter degrees in Celsius: '))
-fahrenheit = (9 / 5) * celsius + 32 
-print (celsius,'Celsius is',fahrenheit,'Fahrenheit.')
+#2.1 Convert Celsius to Fahrenheit
+if assignment ==1:
+    celsius = eval(input('Enter degrees in Celsius: '))
+    fahrenheit = (9 / 5) * celsius + 32 
+    print (celsius,'Celsius is',fahrenheit,'Fahrenheit.')
 
 #2.2 Compute the volume of a cylinder
-
-pi = 3.14
-radius, length = eval (input ('Enter the radius and length of a cylinder: '))
-area = pi*radius**2
-volume = area*length
-
-print ('The area is',area,'and the volume is',volume,'.')
+if assignment ==2:
+    pi = 3.14
+    radius, length = eval (input ('Enter the radius and length of a cylinder: '))
+    area = pi*radius**2
+    volume = area*length
+    print ('The area is',area,'and the volume is',volume,'.')
 
 #2.4 Convert pounds into kilograms
+if assignment ==4:
+    pounds = eval(input('Enter weight in pounds:'))
+    kilograms = 0.453592 * pounds
+    print (pounds,' pounds is ',kilograms,' kilograms.')
 
-pounds = eval(input('Enter weight in pounds:'))
-kilograms = 0.453592 * pounds
-print (pounds,' pounds is ',kilograms,' kilograms.')
+#2.5 Subtotal and a gratuity rate: 
+if assignment ==5:
+    subtotal,gratuity = eval(input('Enter the subtotal and a gratuity rate:')) 
+    print ('gratuity is',subtotal*gratuity/100,'and total is',subtotal+subtotal*gratuity/100)
+
